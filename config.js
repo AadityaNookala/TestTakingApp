b0754c0613c01d8bd1b9fefba47dceab3c4b2a77
AadityaNookala <aaditya.nookala2012@gmail.com>
2024-10-15 07:22:10 -0500

    CHanged config

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
-// export const baseUrl =
-// "https://us-central1-allprojects-424621.cloudfunctions.net/spellings-function";
-// export const baseUrlScheduler =
-//   "https://us-central1-allprojects-424621.cloudfunctions.net/scheduler-function";
-// export const bucketName = "test-taking-bucket";
-export const baseUrl = "http://127.0.0.1:8000";
-export const baseUrlScheduler = "http://127.0.0.1:3000";
-export const bucketName = "test-taking-bucket-dev";
+export const baseUrl =
+  "https://us-central1-allprojects-424621.cloudfunctions.net/spellings-function";
+export const baseUrlScheduler =
+  "https://us-central1-allprojects-424621.cloudfunctions.net/scheduler-function";
+export const bucketName = "test-taking-bucket";
+// export const baseUrl = "http://127.0.0.1:8000";
+// export const baseUrlScheduler = "http://127.0.0.1:3000";
+// export const bucketName = "test-taking-bucket-dev";
 
 export const arrOfPuncs = [
   ",",
