0cd78116c38a890aec75cc23aa73d42d6f880350
AadityaNookala <aaditya.nookala2012@gmail.com>
2024-11-16 17:10:58 -0600

    Changed baseUrl

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-// export const baseUrl =
-//   "https://us-central1-allprojects-424621.cloudfunctions.net/spellings-function";
-// export const baseUrlScheduler =
-//   "https://us-central1-allprojects-424621.cloudfunctions.net/scheduler-function";
-export const baseUrl = "http://127.0.0.1:8000";
-export const baseUrlScheduler = "http://127.0.0.1:3000";
+export const baseUrl =
+  "https://us-central1-allprojects-424621.cloudfunctions.net/spellings-function";
+export const baseUrlScheduler =
+  "https://us-central1-allprojects-424621.cloudfunctions.net/scheduler-function";
+// export const baseUrl = "http://127.0.0.1:8000";
+// export const baseUrlScheduler = "http://127.0.0.1:3000";
 export const arrOfPuncs = [
   ",",
   ".",
